dd98ab3ee903653d6f3f62f79e99b6d55145b027
Mostafa Maki <mostafa_m9@yahoo.com>
2022-12-04 19:06:37 -0800

    updated login and signup code

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
@@ -32,12 +32,12 @@ function App() {
       <Router>
         <>
           <Navbar />
-          <Routes>
+          <Switch>
             <Route exact path="/" element={<SearchBooks />} />
             <Route exact path="/saved" element={<SavedBooks />} />
             <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
 
-          </Routes>
+          </Switch>
         </>
       </Router>
     </ApolloProvider>
